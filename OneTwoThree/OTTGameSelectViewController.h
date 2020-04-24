093455a908a0ac1938b523ed0e0b24b0a42af9fb
Pete Victoratos <pete.victoratos@aboutobjects.com>
2020-04-24 13:36:43 -0400

    added another to do and a button

--- OneTwoThree/OTTGameSelectViewController.h
+++ OneTwoThree/OTTGameSelectViewController.h
@@ -8,10 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-NS_ASSUME_NONNULL_BEGIN
-
 @interface OTTGameSelectViewController : UIViewController
-
 @end
 
-NS_ASSUME_NONNULL_END
--- OneTwoThree/OTTGameSelectViewController.m
+++ OneTwoThree/OTTGameSelectViewController.m
@@ -10,12 +10,13 @@
 #import <CoreGraphics/CoreGraphics.h>
 
 @interface OTTGameSelectViewController ()
-@property (weak, nonatomic) IBOutlet UIImageView *OTTLogo;
 @property (weak, nonatomic) IBOutlet UIButton *addition;
 @property (weak, nonatomic) IBOutlet UIButton *subtraction;
 @property (weak, nonatomic) IBOutlet UIButton *multiplication;
 @property (weak, nonatomic) IBOutlet UIButton *division;
+
 @property (weak, nonatomic) IBOutlet UISegmentedControl *difficultyControl;
+@property (weak, nonatomic) IBOutlet UIButton *startButton;
 
 @end
 
@@ -29,6 +30,13 @@
 //TODO: find a way for segmented control to talk to the game modes
         //it needs to set the labels appropriately
         //it needs to change the difficulty of the game
+
+//TODO: start button function
+    //create a local variable that when empty, does nothing on button press
+    //but once the user has tapped one of the operators it sets the variable to that operator
+    //start button then uses this to determine the type of game that is displayed on the game screen
+    //
+    //name: startButtonPressed variables: operatorIdentifier, Difficulty
 
 //TODO: buttons need to showTouchWhenHighlighted and be formatted
 
